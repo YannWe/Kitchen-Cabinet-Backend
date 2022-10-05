c66a7ffac69a20c6ed051d8c89a9fd39e9daa0d9
ywe <janno4@gmail.com>
2022-10-05 15:06:56 +0200

    changed PORT to 3000 from 8002 to match cyclic port

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ mongoose.connection.on("error", (error) => {
 });
 
 
-const PORT = process.env.PORT || 8002;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
 
