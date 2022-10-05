ec998a31e1528ecbd8edd372921acdfabd6486b4
ywe <janno4@gmail.com>
2022-10-05 15:35:12 +0200

    redeploying

--- server.js
+++ server.js
@@ -40,5 +40,5 @@ mongoose.connection.on("error", (error) => {
 });
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
